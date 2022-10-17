83cbf878a474e50a1e9f5a0a3eeb0171092f7be2
icis-04 <chidexiwuaku@gmail.com>
2022-10-16 21:49:25 -0700

    html rendered

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+    Script that routes url to functions in flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ renders "Hello HBNB" """
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ renders "HBNB" """
+    return "HBNB"
+
+@app.route('/c/<text>',strict_slashes=False)
+def c_display(text):
+    """ renders C and whatever is inside text """
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text="is_cool"):
+    """ renders Python + whatever is inside text """
+    txt = text.replace('_', ' ')
+    return "Python {}".format(txt)
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def integer(n):
+    """ renders the number n and 'is a number' """
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
