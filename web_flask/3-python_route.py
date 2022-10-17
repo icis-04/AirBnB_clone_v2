3a06bc8efbbb90e20b22eb93f7299bb3971639bd
icis-04 <chidexiwuaku@gmail.com>
2022-10-16 20:46:41 -0700

    routing is fun

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    Script that routes url to functions in flask
+"""
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ renders "Hello HBNB" """
+    return "Hello HBNB!"
+@app.route('/hbnb', strict_slashes=False)
+def hello_hbnb():
+    """ renders "HBNB" """
+    return "HBNB"
+
+@app.route('/c/<text>',strict_slashes=False)
+def c_display(text):
+    """ renders C and whatever is inside text """
+    txt = text.replace("_", " ")
+    return "C {}".format(txt)
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_display(text="is_cool"):
+    """ renders Python + whatever is inside text """
+    txt = text.replace('_', ' ')
+    return "Python {}".format(txt)
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
